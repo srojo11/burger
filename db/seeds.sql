46780f0a9fe0df5817794846f4b74f9ab6b0d9c7
Steven Rojsuontikul <steven.rojsuontikul@gmail.com>
2017-09-07 19:39:34 -0400

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', NULL);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', NULL);
+INSERT INTO burgers (burger_name, devoured) VALUES ('tofu burger', NULL);
